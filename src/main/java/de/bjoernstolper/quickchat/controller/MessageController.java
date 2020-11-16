1fe7a7d729c36ccdf7dd66a59214f42f739e2dbd
Björn Stolper <bjoern.stolper@neuland-bfi.de>
2020-11-16 12:20:37 +0100

    Refactor to use entity of ServerSentEvent

--- src/main/java/de/bjoernstolper/quickchat/controller/MessageController.java
+++ src/main/java/de/bjoernstolper/quickchat/controller/MessageController.java
@@ -3,8 +3,8 @@ package de.bjoernstolper.quickchat.controller;
 import de.bjoernstolper.quickchat.model.Message;
 import de.bjoernstolper.quickchat.service.MessageService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +14,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/messages/")
@@ -27,7 +28,7 @@ public class MessageController {
 
     @PostMapping
     public Mono<ResponseEntity<Message>> addMessage(@RequestBody Message message) {
-        message.setId(null);    //prevent IDs from outside
+        message.setId(null);    //prevent IDs from outside. TODO: Refactor to not use Message here. For Simplicity we use it for retrieval of username
         return messageService.create(message)
                 .map(m -> ResponseEntity
                         .created(URI.create("/messages/" + m.getId()))
@@ -35,9 +36,15 @@ public class MessageController {
                 .defaultIfEmpty(ResponseEntity.status(HttpStatus.CONFLICT).build());
     }
 
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Message> getMessageStream() {
-        return messageService.getAll();
+    @GetMapping
+    public Flux<ServerSentEvent<Message>> getMessageStream() {
+        return messageService.getAll()
+                .map(sequence -> ServerSentEvent.<Message>builder()
+                        .id(String.valueOf(sequence.hashCode()))
+                        .event("message")
+                        .data(sequence)
+                        .retry(Duration.ofMillis(3000))
+                        .build());
     }
 
 }
